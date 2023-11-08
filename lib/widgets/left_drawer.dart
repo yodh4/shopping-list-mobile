c512f0b76078b442fa9f49d4ab30ce91699e195b
yodh4 <cyrilus2004@gmail.com>
2023-11-08 16:28:53 +0700

    Memindahkan widget  ShopCard ke shop_card.dart

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -11,56 +11,57 @@ class LeftDrawer extends StatelessWidget {
       child: ListView(
         children: [
           const DrawerHeader(
-  decoration: BoxDecoration(
-    color: Colors.indigo,
-  ),
-  child: Column(
-    children: [
-      Text(
-        'Shopping List',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-      ),
-      Padding(padding: EdgeInsets.all(10)),
-      Text(
-        "Catat seluruh keperluan belanjamu di sini!",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
+            decoration: BoxDecoration(
+              color: Colors.indigo,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Shopping List',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "Catat seluruh keperluan belanjamu di sini!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
-    ],
-  ),
-),
           ListTile(
-  leading: const Icon(Icons.home_outlined),
-  title: const Text('Halaman Utama'),
-  // Bagian redirection ke MyHomePage
-  onTap: () {
-    Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MyHomePage(),
-        ));
-  },
-),
-ListTile(
-  leading: const Icon(Icons.add_shopping_cart),
-  title: const Text('Tambah Produk'),
-  // Bagian redirection ke ShopFormPage
-  onTap: () {
-    Navigator.push(
-        context, 
-        MaterialPageRoute(builder: (context) => const ShopFormPage(),));
-  },
-),
-          
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Halaman Utama'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_shopping_cart),
+            title: const Text('Tambah Produk'),
+            // Bagian redirection ke ShopFormPage
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ShopFormPage(),
+                  ));
+            },
+          ),
         ],
       ),
     );
--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -8,3 +8,56 @@ class ShopItem {
 
   ShopItem(this.name, this.icon, this.color);
 }
+
+class ShopCard extends StatelessWidget {
+  final ShopItem item;
+
+  const ShopCard(this.item, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color,
+      child: InkWell(
+        // Area responsive terhadap sentuhan
+        onTap: () {
+          // Memunculkan SnackBar ketika diklik
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(SnackBar(
+          content: Text("Kamu telah menekan tombol ${item.name}!")));
+
+    // Navigate ke route yang sesuai (tergantung jenis tombol)
+    if (item.name == "Tambah Produk") {
+      Navigator.push(
+        context, 
+        MaterialPageRoute(builder: (context) => const ShopFormPage(),));
+    }
+
+  },
+        child: Container(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
